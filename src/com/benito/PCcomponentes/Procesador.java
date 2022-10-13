c0432ed81b3e84123369ee7d9b67947e83b7218e
benjaminAA123 <115726240+benjaminAA123@users.noreply.github.com>
2022-10-13 18:35:54 +0200

    primer proyecto

---
+++ src/com/benito/PCcomponentes/Procesador.java
@@ -0,0 +1,94 @@
+package com.benito.PCcomponentes;
+
+/**
+* Clase  Procesador
+*/
+
+public class Procesador {
+
+    /**
+    * Atributos propiedades variables
+    */
+
+    private Integer id;
+    private String Marca;
+f
+    /**
+    * Metodos, funciones, comportamiento
+     */
+
+    public Integer getId(){
+        return id;
+    }
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public String getMarca(){
+        return Marca;
+    }
+public void getMarca(marca){
+
+}
+    public getFabricante (){
+        return Fabricante;
+    }
+    public void getFabricante(String Fabricante)
+
+        package com.benito.PCcomponentes
+    public class Main {
+        public static void main (String[] args) {
+            System.out.println("iniciando...");
+
+            /**
+             * *codigo a ejecutar por un administrativo
+             * codigo para introducir informacion de proceso
+             */
+            amd.setID(1);
+            amd.setMarca("Riazen 6400");
+            amd.setFabricante("AMD");
+            System.out.println(amd.getMarca());
+            System.out.println(amd.getFabricante());
+
+            Procesador pentium = new Procesador();
+            pentium.setID(2),
+            pentium.setMarca("i7");
+            pentium.setFabricante("Intel")
+
+        }
+    }
+
+    }
+ package com.benito.PCcomponentes;
+    public class MotherBoard {
+/**
+ * Atributos, prpiedades , variables
+ */
+private Interger ID;
+private String marca;
+private String fabricante;
+/**
+ * Secciones comportamiento, metodos
+ */
+public Integer getID(){
+    return ID;
+}
+public void setID (Integer ID){
+    this.ID = ID;
+}
+
+public String getMarca(){
+    return Marca;
+}
+public void setFabricante(){
+    return fabricante;
+
+}
+public void setFabricante(String){
+
+        public void  setFabricante(String fabricante){
+        this.fabricante = fabricante;
+    }
+
+    }
+
