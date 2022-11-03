6f478222a5ded5c33e742e89fabc0f252cf459bf
benjaminAA123 <115726240+benjaminAA123@users.noreply.github.com>
2022-11-03 17:32:05 +0100

    PC_COMPONENTES_partes

---
+++ src/com/benito/PCcomponentes/Motherboard.java
@@ -0,0 +1,35 @@
+package com.benito.PCcomponentes;
+
+public class Motherboard {
+    private Integer id;
+    public String marca;
+    private String fabricante;
+    public String potencia;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getPotencia() { return potencia; }
+    public void setPotencia(String potencia) { this.potencia = potencia; }
+}
